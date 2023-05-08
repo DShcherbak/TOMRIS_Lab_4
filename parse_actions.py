477e7f3a0479b52d3ca50e17ca5d37eaf0fb98d5
Ivan Kushnarenko <ivan.kushnarenko@gmdhshell.com>
2023-05-08 03:34:16 +0300

    added actions: parse only binary expr

---
+++ parse_actions.py
@@ -0,0 +1,89 @@
+import commands
+import term
+
+
+def identifier(s) -> term.Term:
+    return term.Term(name=s[0])
+
+
+def integer(*args) -> term.Term:
+    token = args[2]
+    return term.Term(value=int(token[0]))
+
+
+def _term_operator(s) -> term.TermOperator:
+    if s == '+':
+        return term.TermOperator.Plus
+    elif s == '-':
+        return term.TermOperator.Minus
+    elif s == '*':
+        return term.TermOperator.Multiply
+    elif s == '/':
+        return term.TermOperator.Divide
+    else:
+        raise ValueError('Cannot convert operator from string')
+
+
+def math_expr(*args) -> term.Term:
+    tokens = args[2][0]
+    return term.Term(lhs=tokens[0],
+                     operator=_term_operator(tokens[1]),
+                     rhs=tokens[2])
+
+
+def assignment(*args) -> commands.Command:
+    tokens = args[2][0]
+    assignment_dic = {tokens[0].name: tokens[2]}
+    return commands.Command(type=commands.CommandType.Do, args=assignment_dic)
+
+
+def _bool_operator(s) -> commands.ConditionType:
+    if s == '==':
+        return commands.ConditionType.EQ
+    elif s == '>':
+        return commands.ConditionType.GT
+    elif s == '<':
+        return commands.ConditionType.LT
+    elif s == '>=':
+        return commands.ConditionType.GQ
+    elif s == '<=':
+        return commands.ConditionType.LQ
+    elif s == '!=':
+        return commands.ConditionType.NQ
+    else:
+        raise ValueError("Cannot parse boolean operator")
+
+
+def condition(*args) -> commands.Condition:
+    tokens = args[2][0]
+    return commands.Condition(lhs=tokens[1],
+                              operator=_bool_operator(tokens[2]),
+                              rhs=tokens[3])
+
+
+def if_statement(*args) -> commands.Command:
+    tokens = args[2][0]
+    if_block = tokens[0]
+    else_block = None
+    if len(tokens) == 2:    # both if and else blocks
+        else_block = tokens[1]
+    return commands.Command(
+        type=commands.CommandType.If,
+        condition=if_block[1],  # list of commands
+        args=if_block[3: -1],
+        elseCommands=else_block[2: -1] if else_block is not None else None  # list of commands
+    )
+
+
+def while_statement(*args) -> commands.Command:
+    tokens = args[2][0]
+    return commands.Command(
+        type=commands.CommandType.While,
+        condition=tokens[1],
+        args=tokens[3: -1]  # list of commands
+    )
+
+
+def commands_list(*args) -> list[commands.Command]:
+    tokens = args[2][0]
+    return tokens[1:]
--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import pyparsing as prs
 
 from enum import Enum
 
+import parse_actions as ac
 
 class Keyword(Enum):
     IF = 0
@@ -11,45 +12,46 @@ class Keyword(Enum):
 
 keywords: dict[Keyword, str] = {Keyword.IF: 'if', Keyword.ELSE: 'else', Keyword.WHILE: 'while'}
 
-_identifier = prs.Word(prs.alphas).set_parse_action()
-_integer = prs.pyparsing_common.integer
+_identifier = prs.Word(prs.alphas).set_parse_action(ac.identifier)
+_integer = prs.pyparsing_common.integer.set_parse_action(ac.integer)
 _var_or_num = _identifier | _integer
 math_op = prs.one_of(['+', '-', '*', '/'])
-_math_expr = prs.Group(_var_or_num + math_op + _var_or_num)('math_expr')
+_math_expr = prs.Group(_var_or_num + math_op + _var_or_num)('math_expr').set_parse_action(ac.math_expr)
 
 _operand = _math_expr | _integer | _identifier
 
-_assignment = prs.Group(_identifier + prs.Literal(':=') + _operand + prs.Literal(';'))("assignment")
+_assignment = prs.Group(_identifier + prs.Literal(':=') + _operand + prs.Literal(';')).set_parse_action(ac.assignment)
 
 _bool_op = prs.one_of(['<', '<=', '>', '>=', '==', '!='])
-_bool_expr = prs.Group('(' + _operand + _bool_op + _operand + ')')('bool_expr')
+_bool_expr = prs.Group('(' + _operand + _bool_op + _operand + ')')('bool_expr').set_parse_action(ac.condition)
 
 _if_statement = prs.Forward()
 _while_loop = prs.Forward()
 
-_command = prs.Group(_if_statement | _while_loop | _assignment)('command')
+_command = _if_statement | _while_loop | _assignment
 
-_if_block = prs.Group(keywords[Keyword.IF] + _bool_expr + '{' + prs.OneOrMore(_command) + '}')('if_block')
-_else_block = prs.Group(keywords[Keyword.ELSE] + '{' + prs.OneOrMore(_command) + '}')('else_block')
-_if_statement << prs.Group(_if_block + prs.Optional(_else_block))("")('if_statement')
+_if_block = prs.Group(
+    keywords[Keyword.IF] + _bool_expr +
+    '{' + prs.OneOrMore(_command) + '}'
+).set_results_name("if_block")
 
-_while_loop << prs.Group(keywords[Keyword.WHILE] + _bool_expr + '{' + prs.OneOrMore(_command) + '}')('while_loop')
+_else_block = prs.Group(
+    prs.Literal(keywords[Keyword.ELSE]) +
+    '{' + prs.OneOrMore(_command) + '}'
+)('else_block').set_results_name('else_block')
+
+_if_statement << prs.Group(_if_block + prs.Optional(_else_block))('if_statement').set_parse_action(ac.if_statement)
+
+_while_loop << prs.Group(keywords[Keyword.WHILE] + _bool_expr + '{' + prs.OneOrMore(_command) + '}')('while_loop')\
+    .set_parse_action(ac.while_statement)
 
 _args_list = prs.delimited_list(_identifier)
 _params = prs.Group('(' + prs.Optional(_args_list) + ')')
 _program_name = prs.Group(_identifier + _params)
 
-_program_code = prs.Group(_program_name + prs.OneOrMore(_command))('program')
+_program_code = prs.Group(_program_name + prs.OneOrMore(_command))('program').set_parse_action(ac.commands_list)
 
 
 def parse(code: str):
     prs.ParserElement.ignoreWhitespace = True
     return _program_code.parseString(code)
-
-
-def get_commands(s: str):
-    result = parse(s)
-    for g in result:
-        print(g)
-
-
